ae66d58f9d8af0f2a25e09f39671c4668689fd4a
Hellon Canella Machado <helloncanella@gmail.com>
2015-04-18 19:56:16 -0300

    blackboard pattern created

--- blackboard/js/script.js
+++ blackboard/js/script.js
@@ -1,142 +1,107 @@
-function Scenario(){
-
-    this.fps = 100;
-    this.worldHeight = 10; //Real height of represented world.
-    this.worldWidth = null; //To be determined
-
-    this.body = null;
-
-    this.context = null;
-    this.canvasWidth=null;
-    this.canvasHeight=null;
-
-    this.scale = null; //ratio between canvas and world dimensions.
-
-    //World variables
-    this.gravity=null;
-    this.world=null;
+function Blackboard (){
+    var self = this;
+   
+    //canvas variables
+    this.canvas    = null;
+    this.context   = null;
     
-        
-    this.start=function(){
-	this.setup();
-	this.tick();
-    }
+    //events variables
+    this.pressed   = null;
+    this.unpressed = null;
+    this.moving    = null;
 
+    
     this.setup = function(){
-	this.fit();
-	this.setWorld();
-	this.insertBody();
+	
+	this.setCanvas();
+	this.inputHandler();
+
+	var self = this;	
+	$(window).resize(function(){
+	    self.setCanvas();
+	});	
     }
 
-    this.fit = function(){
-
-	var canvas = document.createElement("CANVAS");
-	document.body.appendChild(canvas);
-	this.context = canvas.getContext("2d");
-
-	var w = $(window).width();
-	var h = $(window).height();
-
-	$('canvas').attr('width',w);
-	this.canvasWidth = $('canvas').attr('width');
+    this.setCanvas = function(){
 	
+	$('canvas').remove();	//Removing previous canvas
+	$('body').append('<canvas></canvas>');//Appending new canvas
 	
-	$('canvas').attr('height',h);
-	this.canvasHeight = $('canvas').attr('height');
-	
-	//Converting screen and world size
-	this.scale =this.canvasHeight/this.worldHeight;
-	this.worldWidth = this.canvasWidth/this.scale;
-	
+	var canvas = this.canvas = $('canvas')[0];
+	var context= this.context = canvas.getContext('2d');
+	console.log(canvas+","+context);
 
+	var windowWidth  = $(window).width();
+	var windowHeight = $(window).height();
+	
+	$('canvas').width(windowWidth).height(windowHeight);
 
+	//Drawing background
+	var background = new Image();
+	background.src = "../blackboard/assets/chalkboard.jpg";
+	
+	var pattern = context.createPattern(background,"repeat");
+	context.rect(0,0,canvas.width,canvas.height);
+	context.fillStyle = pattern;
+	context.fill();
+	
     }
 
 
-    this.setWorld =  function(){
-	this.gravity = new Vec(0,-10);
+    this.inputHandler = function(){
+	//Verifying if there is support for touch events
+	var touchSupported = Modernizr.touch;
+	if(touchSupported){
+	    this.pressed   = "touchstart";
+	    this.unpressed = "touchend";
+	    this.moving    = "touchmove";
+	}
+	else{
+	    this.pressed   = "mousedown";
+	    this.unpressed = "mouseup";
+	    this.moving    = "mousemove";
+	}
 
-	var sleep = false; //If true the body will sleep when reach static body
-	this.world = new World(this.gravity,sleep); 
-    }
-
-
-    this.insertBody = function(){
-
-	var square = new BodyDef();
-	square.type = Body.b2_dynamicBody;
-	square.position.Set(0,this.worldHeight);
-	
-	var fixture = new FixtureDef;
-	fixture.density = 1.0;
-	fixture.friction = 10000;
-	fixture.restituition = 0.2;
-	fixture.shape = new PolygonShape();
-	fixture.shape.SetAsBox(0.001,0.001);
-
-	this.body = this.world.CreateBody(square);
-
-	this.body.CreateFixture(fixture);
-
-	
+	var self = this;
+	$(document).bind(self.pressed,self.onPressed());
 
     }
- 
 
-    this.tick = function(){
+    this.onPressed = function(){
+	
+	var self = this;
 
-	this.world.Step(1/this.fps,8,3);
-	this.world.ClearForces();
-	this.draw();
+	this.onMoving();
 	
-	var that = this;
-	setTimeout(function(){that.tick()}, 1000/this.fps);
-	
-	
-   }
+	$(document).bind(this.unpressed,this.onUnpressed());
+	$(document).bind(this.moving,this.onMoving());
 
-    this.draw = function(){
+	return function(){
+	    console.log("pressed");
+	}
 	
-	//Cleaning the canvas
-	this.context.clearRect(0 , 0 , this.canvasWidth , this.canvasHeight);
-	
-	var worldPosition = this.body.GetPosition();
-       
-	//change world to canvas referential frame
-	var canvasPosition = this.transformReferentialFrame(worldPosition);
-      
-	console.log("Canvas Position");
-	console.log(canvasPosition.x+","+canvasPosition.y);
-   
-	console.log("World Position");
-	console.log(worldPosition.x+","+worldPosition.y);
-
-	var image = img_res('kawaii.png');
-	
-	this.context.drawImage(image,canvasPosition.x,canvasPosition.y,200,200);
-   
     }
 
-   this.transformReferentialFrame = function(worldPosition){
-      
-        
-       //the (0,0) canvas point is the top left corner 
-       var canvasPosition = {
-	   //Mathematically, the vector canvasPositon = scale * worldPosition
-	   x: worldPosition.x * this.scale,
-	   y: Math.abs(this.canvasHeight - worldPosition.y * this.scale)
-       
-       }
+    this.onUnpressed = function(){
+	$(document).unbind(this.moving);
 
-       return canvasPosition;
-   
 
-   }    
+	return function(){
+	    console.log("unpressed");
+	}
+    }
 
-   
+    this.onMoving = function(){
+	return function(event){
+	    console.log("moving");
+	    event.preventDefault();
+	    return false;
+	}
+    }
 
 
 }
 
-var board = new Scenario();
-board.start();
+
+var blackboard = new Blackboard();
+blackboard.setup();
